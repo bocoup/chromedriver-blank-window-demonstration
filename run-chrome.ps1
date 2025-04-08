128e89ceea7916c61a9a87ff0d3c32cc3819379c
Mike Pennisi <mike@mikepennisi.com>
2025-04-07 21:24:39 -0400

    Start Chrome directly from the command line

--- run-chrome.ps1
+++ run-chrome.ps1
@@ -28,16 +28,18 @@ function Wait-For-HTTP-Response {
   Write-Output "$status after $sleeps tries"
 }
 
-Write-Output "Starting chromedriver"
-$webdriverprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO --enable-chrome-logs *>&1 >$using:pwd\webdriver-log.txt }
-Write-Output "Waiting for localhost:4444 to start from chromedriver"
-Wait-For-HTTP-Response -RequestURL http://localhost:4444/
-
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Method 'POST' `
-  -Body '{"capabilities": {"alwaysMatch": { "browserName": "chrome" } } }' `
-  -Uri http://localhost:4444/session >> $pwd\http-testing-log.txt
+#Write-Output "Starting chromedriver"
+#$webdriverprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO --enable-chrome-logs *>&1 >$using:pwd\webdriver-log.txt }
+#Write-Output "Waiting for localhost:4444 to start from chromedriver"
+#Wait-For-HTTP-Response -RequestURL http://localhost:4444/
+#
+#Invoke-WebRequest `
+#  -UseBasicParsing `
+#  -Method 'POST' `
+#  -Body '{"capabilities": {"alwaysMatch": { "browserName": "chrome" } } }' `
+#  -Uri http://localhost:4444/session >> $pwd\http-testing-log.txt
+mkdir chrome-profile
+Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--user-data-dir=./chrome-profile --no-first-run https://example.com"
 
 Add-Type -AssemblyName System.Windows.Forms,System.Drawing
 $screens = [Windows.Forms.Screen]::AllScreens
